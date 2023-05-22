f9ca700dbd2ff17ce9cd6fbcc9ea8c673928e5a7
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-05-22 13:26:00 +0300

    add format to options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
+
 import { Command } from 'commander';
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-
-program.command('gendiff')
-  .description('Compares two configuration files and shows a difference')
+  .version('0.0.1')
+  .argument('<type>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
