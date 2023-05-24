cc799930daeb1b7d1f01e395f8f1f7e7237a9e5f
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-05-24 11:22:12 +0300

    add filepath args

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .argument('<type>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file');
 
 program.parse();
