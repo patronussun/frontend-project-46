bfbf6711a8a6ff5c479853ff4b7576c16f5e3756
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-05-24 14:54:02 +0300

    update arguments in gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,6 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1>', 'path to first file')
-  .argument('<filepath2>', 'path to second file');
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
