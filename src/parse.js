9cd3600a59ffb2952c58a9b3b536a1049d7db3a2
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-05-29 13:18:37 +0300

    wip

--- src/parse.js
+++ src/parse.js
@@ -1,13 +1,46 @@
 import { readFileSync } from 'fs';
 import _ from 'lodash';
 
+// const parseFile = (filepath) => {
+//   const rawData = readFileSync(filepath);
+//   const object = JSON.parse(rawData);
+//   return object;
+// };
+
 const parseFile = (filepath) => {
   const rawData = readFileSync(filepath);
   const object = JSON.parse(rawData);
   return object;
 };
 
-const compare = (filepath1, filepath2) => {
-  const sortedObject1 = _.sortBy(parseFile(filepath1));
-  const sortedObject2 = _.sortBy(parseFile(filepath2));
-}
+const compare = () => {
+  const result = [];
+  const object1 = parseFile('__fixtures__/file1.json');
+  const object2 = parseFile('__fixtures__/file2.json');
+  const sortedKeys1 = _.sortBy(Object.keys(object1));
+  const sortedKeys2 = _.sortBy(Object.keys(object2));
+
+  for (const key of sortedKeys1) {
+    if (sortedKeys2.includes(key)) {
+      if (object1.key === object2.key) {
+        result.push(`  ${key}: ${object1.key}`);
+      }
+      if (object1.key !== object2.key) {
+        result.push(`- ${key}: ${object1.key}`);
+        result.push(`+ ${key}: ${object2.key}`);
+      }
+    }
+    else if (!sortedKeys2.includes(key)) {
+      result.push(`- ${key}: ${object1.key}`);
+    }
+  }
+
+  for (const key of sortedKeys2) {
+    if (!sortedKeys1.includes(key)) {
+      result.push(`+ ${key}: ${object2.key}`);
+    }
+  }
+  console.log(result);
+};
+
+compare();
