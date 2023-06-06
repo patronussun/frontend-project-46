fbb951bc44541c02ae6e28b77db6a3f82701c2a8
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-06-06 18:31:59 +0300

    compare function

--- src/parse.js
+++ src/parse.js
@@ -14,7 +14,7 @@ const parseFile = (filepath) => {
 };
 
 const compare = () => {
-  const result = [];
+  const result = ['{'];
   const object1 = parseFile('__fixtures__/file1.json');
   console.log('object1', object1);
   const object2 = parseFile('__fixtures__/file2.json');
@@ -26,24 +26,24 @@ const compare = () => {
 
   sortedKeys.forEach((key) => {
     if (keys1.includes(key) && keys2.includes(key) && object1[key] === object2[key]) {
-      return console.log(`  ${key}: ${object1[key]}`);
+      result.push(`  ${key}: ${object1[key]}`);
     }
 
     if (!keys2.includes(key)) {
-      console.log(`- ${key}: ${object1[key]}`);
+      result.push(`- ${key}: ${object1[key]}`);
     }
 
     if (!keys1.includes(key)) {
-      console.log(`+ ${key}: ${object2[key]}`);
+      result.push(`+ ${key}: ${object2[key]}`);
     }
 
     if (keys1.includes(key) && keys2.includes(key) && object1[key] !== object2[key]) {
-      console.log(`- ${key}: ${object1[key]}`);
-      console.log(`+ ${key}: ${object2[key]}`);
+      result.push(`- ${key}: ${object1[key]}`);
+      result.push(`+ ${key}: ${object2[key]}`);
     }
   });
-
-  console.log(result.join('\n'));
+  result.push('}');
+  return result.join('\n');
 };
 
-compare();
+console.log(compare());
